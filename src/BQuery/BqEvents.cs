20b211b5a14664439f15d90b3ce150a341c3392d
zxyao <zxyao145@gmail.com>
2026-03-10 13:30:43 +0800

    Generate window event slot dictionary

--- src/BQuery/BqEvents.cs
+++ src/BQuery/BqEvents.cs
@@ -6,7 +6,12 @@ namespace BQuery;
 /// </summary>
 public partial class BqEvents
 {
+    private abstract class EventSlot
+    {
+    }
+
     private sealed class EventSlot<T>
+        : EventSlot
     {
         private Action<T>? _syncHandlers;
         private Func<T, Task>? _asyncHandlers;
@@ -49,6 +54,53 @@ public partial class BqEvents
         private static async Task InvokeAsyncHandler(Func<T, Task> handler, T args)
         {
             await handler(args);
+        }
+    }
+
+    private sealed class EventSlot<T1, T2>
+        : EventSlot
+    {
+        private Action<T1, T2>? _syncHandlers;
+        private Func<T1, T2, Task>? _asyncHandlers;
+
+        public event Action<T1, T2>? Sync
+        {
+            add => _syncHandlers += value;
+            remove => _syncHandlers -= value;
+        }
+
+        public event Func<T1, T2, Task>? Async
+        {
+            add => _asyncHandlers += value;
+            remove => _asyncHandlers -= value;
+        }
+
+        public async Task InvokeAsync(T1 arg1, T2 arg2)
+        {
+            if (_syncHandlers is not null)
+            {
+                foreach (var handler in _syncHandlers.GetInvocationList().Cast<Action<T1, T2>>())
+                {
+                    handler(arg1, arg2);
+                }
+            }
+
+            if (_asyncHandlers is not null)
+            {
+                List<Task>? pendingTasks = [];
+
+                foreach (var handler in _asyncHandlers.GetInvocationList().Cast<Func<T1, T2, Task>>())
+                {
+                    pendingTasks.Add(InvokeAsyncHandler(handler, arg1, arg2));
+                }
+
+                await Task.WhenAll(pendingTasks);
+            }
+        }
+
+        private static async Task InvokeAsyncHandler(Func<T1, T2, Task> handler, T1 arg1, T2 arg2)
+        {
+            await handler(arg1, arg2);
         }
     }
 }
